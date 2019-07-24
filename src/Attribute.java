ddda814eb5efc08ffdfb3a78c7e9fa9fe8bf9944
Matt <matt.j.koch@gmail.com>
2019-07-23 20:58:02 -0500

    implement year, date, datetime

--- src/Attribute.java
+++ src/Attribute.java
@@ -1,3 +1,6 @@
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 /*
     Attribute class denotes each cell in the table (Datatype and value)
 */
@@ -28,6 +31,21 @@ public class Attribute
         case BIGINT: this.fieldValue =  Long.valueOf(ByteConvertor.longFromByteArray(fieldValuebyte)).toString(); break;
         case FLOAT: this.fieldValue = Float.valueOf(ByteConvertor.floatFromByteArray(fieldValuebyte)).toString(); break;
         case DOUBLE: this.fieldValue = Double.valueOf(ByteConvertor.doubleFromByteArray(fieldValuebyte)).toString(); break;
+        case YEAR: this.fieldValue = Integer.valueOf((int)Byte.valueOf(ByteConvertor.byteFromByteArray(fieldValuebyte))+2000).toString(); break;
+        case TIME: this.fieldValue = Integer.valueOf(ByteConvertor.intFromByteArray(fieldValuebyte)).toString(); break;
+        case DATETIME:
+            // YYYY-MM-DD_HH:MM:SS
+            Date rawdatetime = new Date(Long.valueOf(ByteConvertor.longFromByteArray(fieldValuebyte)));
+            this.fieldValue = (rawdatetime.getYear()+1900) + "-" + (rawdatetime.getMonth()+1)
+                + "-" + (rawdatetime.getDate()) + "_" + (rawdatetime.getHours()) + ":"
+                + (rawdatetime.getMinutes()) + ":" + (rawdatetime.getSeconds());
+            break;
+        case DATE:
+            // YYYY-MM-DD
+            Date rawdate = new Date(Long.valueOf(ByteConvertor.longFromByteArray(fieldValuebyte)));
+            this.fieldValue = (rawdate.getYear()+1900) + "-" + (rawdate.getMonth()+1)
+                + "-" + (rawdate.getDate());
+            break;
         case TEXT: this.fieldValue = new String(fieldValuebyte); break;
          default:
          this.fieldValue= new String(fieldValuebyte); break;
@@ -51,6 +69,26 @@ public class Attribute
         case BIGINT: this.fieldValuebyte =  ByteConvertor.longTobytes(Long.parseLong(fieldValue)); break;
         case FLOAT: this.fieldValuebyte = ByteConvertor.floatTobytes(Float.parseFloat(fieldValue)); break;
         case DOUBLE: this.fieldValuebyte = ByteConvertor.doubleTobytes(Double.parseDouble(fieldValue)); break;
+        case YEAR: this.fieldValuebyte = new byte[] { (byte) (Integer.parseInt(fieldValue) - 2000) }; break;
+        case TIME: this.fieldValuebyte = ByteConvertor.intTobytes(Integer.parseInt(fieldValue)); break;
+        case DATETIME:
+            SimpleDateFormat sdftime = new SimpleDateFormat("yyyy/MM/dd_HH:mm:ss");
+            try {
+                Date datetime = sdftime.parse(fieldValue);  
+                this.fieldValuebyte = ByteConvertor.longTobytes(datetime.getTime());              
+            } catch (Exception e) {
+                System.out.println("Could not convert " + fieldValue + " to DATETIME.");
+            }
+            break;
+        case DATE:
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+            try {
+                Date date = sdf.parse(fieldValue);  
+                this.fieldValuebyte = ByteConvertor.longTobytes(date.getTime());              
+            } catch (Exception e) {
+                System.out.println("Could not convert " + fieldValue + " to DATETIME.");
+            }
+            break;
         case TEXT: this.fieldValuebyte = fieldValue.getBytes(); break;
          default:
          this.fieldValuebyte = fieldValue.getBytes(); break;
--- src/ByteConvertor.java
+++ src/ByteConvertor.java
@@ -2,15 +2,19 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.util.List;
 
-/* Conversion class to convert datatypes to byte and vice vers*/
+/* Conversion class to convert datatypes to byte and vice versa*/
 public class ByteConvertor{
-    
-    public static Byte[] intToBytes(final int data) {
-		return byteToBytes(ByteBuffer.allocate(Integer.BYTES).order(ByteOrder.BIG_ENDIAN).putInt(data).array());
-	}
 
+    /* To-byte functions*/
     public static Byte[] byteToBytes(final byte[] data){
         Byte[] result= new Byte[data.length];
+        for(int i=0;i<data.length;i++)
+            result[i] = data[i];
+        return result;
+    }
+
+    public static byte[] Bytestobytes(final Byte[] data){
+        byte[] result= new byte[data.length];
         for(int i=0;i<data.length;i++)
             result[i] = data[i];
         return result;
@@ -24,15 +28,19 @@ public class ByteConvertor{
     {
         return byteToBytes(ByteBuffer.allocate(Short.BYTES).order(ByteOrder.BIG_ENDIAN).putShort(data).array());
     }
-    
-     public static byte[] intTobytes(final int data) {
-		return ByteBuffer.allocate(Integer.BYTES).order(ByteOrder.BIG_ENDIAN).putInt(data).array();
-	}
-    
+
     public static byte[] shortTobytes(final short data)
     {
         return ByteBuffer.allocate(Short.BYTES).order(ByteOrder.BIG_ENDIAN).putShort(data).array();
     }
+    
+    public static Byte[] intToBytes(final int data) {
+		return byteToBytes(ByteBuffer.allocate(Integer.BYTES).order(ByteOrder.BIG_ENDIAN).putInt(data).array());
+	}
+
+     public static byte[] intTobytes(final int data) {
+		return ByteBuffer.allocate(Integer.BYTES).order(ByteOrder.BIG_ENDIAN).putInt(data).array();
+	}
     
      public static byte[] longTobytes(final long data) {
 		return ByteBuffer.allocate(Long.BYTES).putLong(data).array();
@@ -40,52 +48,46 @@ public class ByteConvertor{
 
     public static Byte[] longToBytes(final long data) {
 		return byteToBytes(ByteBuffer.allocate(Long.BYTES).putLong(data).array());
-	}
-
-    public static int intFromByteArray(byte[] bytes) {
-        return ByteBuffer.wrap(bytes).getInt();
-   }
-
-   public static short shortFromByteArray(byte[] bytes) {
-        return ByteBuffer.wrap(bytes).getShort();
-   }
-
-    public static byte byteFromByteArray(byte[] bytes) {
-        return ByteBuffer.wrap(bytes).get();
-    }
-
-    public static long longFromByteArray(byte[] bytes) {
-        return ByteBuffer.wrap(bytes).getLong();
-    }
-
-    public static double doubleFromByteArray(byte[] bytes) {
-        return ByteBuffer.wrap(bytes).getDouble();
     }
     
-    public static float floatFromByteArray(byte[] bytes) {
-        return ByteBuffer.wrap(bytes).getFloat();
+    public static Byte[] floatToBytes(final float data) {
+		return byteToBytes(ByteBuffer.allocate(Float.BYTES).putFloat(data).array());
     }
 
     public static byte[] floatTobytes(final float data) {
 		return (ByteBuffer.allocate(Float.BYTES).putFloat(data).array());
     }
 
-    public static byte[] doubleTobytes(final double data) {
-		return (ByteBuffer.allocate(Double.BYTES).putDouble(data).array());
-    }
-
-    public static Byte[] floatToBytes(final float data) {
-		return byteToBytes(ByteBuffer.allocate(Float.BYTES).putFloat(data).array());
-    }
-    
     public static Byte[] doubleToBytes(final double data) {
 		return byteToBytes(ByteBuffer.allocate(Double.BYTES).putDouble(data).array());
     }
 
-    public static byte[] Bytestobytes(final Byte[] data){
-        byte[] result= new byte[data.length];
-        for(int i=0;i<data.length;i++)
-            result[i] = data[i];
-        return result;
+    public static byte[] doubleTobytes(final double data) {
+		return (ByteBuffer.allocate(Double.BYTES).putDouble(data).array());
+    }
+
+    /* From-byte functions*/
+    public static byte byteFromByteArray(byte[] bytes) {
+        return ByteBuffer.wrap(bytes).get();
+    }
+
+    public static short shortFromByteArray(byte[] bytes) {
+        return ByteBuffer.wrap(bytes).getShort();
+    }
+
+    public static int intFromByteArray(byte[] bytes) {
+        return ByteBuffer.wrap(bytes).getInt();
+    }
+
+    public static long longFromByteArray(byte[] bytes) {
+        return ByteBuffer.wrap(bytes).getLong();
+    }
+
+    public static float floatFromByteArray(byte[] bytes) {
+        return ByteBuffer.wrap(bytes).getFloat();
+    }
+
+    public static double doubleFromByteArray(byte[] bytes) {
+        return ByteBuffer.wrap(bytes).getDouble();
     }
 }
